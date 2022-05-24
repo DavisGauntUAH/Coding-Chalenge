fb782244075cb365aca62aec7e1aabf2bef756ac
DavisGauntUAH <bdg0015@uah.edu>
2022-05-24 16:26:16 -0500

    5/24 eod commit

--- next_permutation/next_perm.py
+++ next_permutation/next_perm.py
@@ -1,5 +1,19 @@
 
 
+def next_perm(pre_perm):
+    for idx in range(len(pre_perm)-1, 0, -1):
+        if pre_perm[idx] < pre_perm[idx-1]:
+            for ldx in range (idx+1, len(pre_perm)):
+                if pre_perm[idx] >= pre_perm[ldx]:
+                    pass  # TODO make swap function
+                elif ldx == len(pre_perm):
+                    pass # reverse everything abovce idx TODO implement reverse
+                    #return
+                pass
+            
+            
+                
+
 def main():
     pass
 
