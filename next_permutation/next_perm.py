275485588d231578b40bc478b71d1520792a2a3e
DavisGauntUAH <bdg0015@uah.edu>
2022-05-25 09:51:42 -0500

    next perm solution complete

--- next_permutation/next_perm.py
+++ next_permutation/next_perm.py
@@ -1,23 +1,39 @@
 
 
 def next_perm(pre_perm):
+    """
+    takes in a list and outputs a list containing the
+    next permutation of the given input list
+    """
+    
     for idx in range(len(pre_perm)-1, 0, -1):
         
         if pre_perm[idx] > pre_perm[idx-1]:
             
             for jdx in range (len(pre_perm)-1, idx-1, -1):
+                
                 if jdx > idx-1:
                     pre_perm[idx-1], pre_perm[jdx] = pre_perm[jdx], pre_perm[idx-1]
                     return reverse(pre_perm, idx)
+                
             return reverse(pre_perm, idx)
+        
     return pre_perm[::-1]
             
 def reverse(r_list, fdx):
+    """
+    Takes in a list and an index position to reverse the list after inclusive
+    returns the list contianing the reversed section
+    """
+        
     r_list = r_list[:fdx] + r_list[:fdx-1:-1]
     return(r_list)
 
 def main():
-    print (f"{next_perm([1,2,3])}")
+    """
+    contains test cases from leet code
+    """
+    print (next_perm([1,2,3]))
     print (next_perm([3,2,1]))
     print (next_perm([1,1,5]))
 
