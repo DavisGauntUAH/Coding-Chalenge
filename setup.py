97f3a4b0293a8d7781d9127eb0f7cd8d2c0a7288
DavisGauntUAH <bdg0015@uah.edu>
2022-05-26 15:37:59 -0500

    make setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='coding-chalenge',
+    version='v1.0.0',
+    author='Davis Gaunt',
+    description='leet code evaluation programs',
+    url='https://github.com/DavisGauntUAH/Coding-Chalenge',
+    license='Apache 2.0',
+    classifiers=[
+        'Framework :: Pytest',
+        'Topics :: Evaluation',
+        'Licesnse :: Freeware',
+        'Programing Launguage :: Python :: 3+',
+    ],
+    package=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True
+    
+)
